a439ca092edc5270047f75e57fb0b72f1c9a1222
Elisabeth Henry <liz.henry@ouvaton.org>
2016-10-06 22:11:31 +0200

    Complete documentation

--- README.md
+++ README.md
@@ -4,6 +4,37 @@ Some text processing functions initially written
 for [Crowbook](https://github.com/lise-henry/crowbook) and moved in a
 separate library (and a more permissive license) so they can be used in other projects.
 
+## Usage ##
+
+Add this to your `Cargo.toml` dependencies section:
+
+```toml
+crowbook-text-processing = "0.1"
+```
+
+## Example ##
+
+```rust
+extern crate crowbook_text_processing;
+
+use crowbook_text_processing::escape::*;
+use crowbook_text_processing::clean::*;
+use crowbook_text_processing::french::FrenchFormatter;
+
+fn main() {
+
+    let s = " Some  string with  too much   whitespaces & around 1% characters that might cause trouble to HTML or LaTeX.";
+    let new_s = remove_whitespaces(s);
+    println!("for HTML: {}", escape_html(new_s.clone()));
+    println!("for LaTeX: {}", escape_tex(new_s));
+    
+    let s = " Une chaîne en français ! On voudrait un résultat « typographiquement correct ».";
+    let new_s = FrenchFormatter::new().format(s);
+    println!("for HTML: {}", escape_nb_spaces(escape_html(new_s.clone())));
+    println!("for LaTeX: {}", escape_nb_spaces_tex(escape_tex(new_s)));
+}
+```
+
 ## Documentation ##
 
 See the
@@ -22,3 +53,10 @@ See [the ChangeLog file](ChangeLog.md).
 
 This is free software, published under the [Mozilla Public License,
 version 2.0](https://www.mozilla.org/en-US/MPL/2.0/).
+
+## Semantic versioning ##
+
+While not yet at version `1.0`, this crates tries to follows semantic versioning in the following way:
+
+* an increase of `x` in `0.x.y` means breaking changes.
+* an increase of `y` in `0.x.y` means non-breaking changes.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with
-// this file, You can obtain one at https://mozilla.org/MPL/2.0/.  
+// this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+//! Some utilities functions for processing texts.
+//!
+//! These functions were originally written for [Crowbook](https://github.com/lise-henry/crowbook),
+//! but have been published on a separate crate and under a less
+//! restrictive license (MPL instead of LGPL) so they can be used
+//! elsewhere.
+//!
+//! # Example
+//!
+//! ```
+//! use crowbook_text_processing::escape::*;
+//! use crowbook_text_processing::clean::*;
+//! use crowbook_text_processing::french::FrenchFormatter;
+//!
+//! let s = " Some  string with  too much   whitespaces & around 1% characters that might cause trouble to HTML or LaTeX.";
+//! let new_s = remove_whitespaces(s);
+//! println!("for HTML: {}", escape_html(new_s.clone()));
+//! println!("for LaTeX: {}", escape_tex(new_s));
+//!
+//! let s = " Une chaîne en français ! On voudrait un résultat « typographiquement correct ».";
+//! let new_s = FrenchFormatter::new().format(s);
+//! println!("for HTML: {}", escape_nb_spaces(escape_html(new_s.clone())));
+//! println!("for LaTeX: {}", escape_nb_spaces_tex(escape_tex(new_s)));
+//! ```
+//!
+//! # Semantic versioning
+//!
+//! While not yet at version `1.0`, this crates tries to follows semantic versioning in the following way:
+//!
+//! * an increase of `x` in `0.x.y` means breaking changes.
+//! * an increase of `y` in `0.x.y` means non-breaking changes.
 extern crate regex;
 #[macro_use] extern crate lazy_static;
 
