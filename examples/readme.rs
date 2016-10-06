1cce6d71442b149a54bca5eab57bfae5b09ac40c
Elisabeth Henry <liz.henry@ouvaton.org>
2016-10-06 22:11:23 +0200

    Add an example

---
+++ examples/readme.rs
@@ -0,0 +1,18 @@
+extern crate crowbook_text_processing;
+
+use crowbook_text_processing::escape::*;
+use crowbook_text_processing::clean::*;
+use crowbook_text_processing::french::FrenchFormatter;
+
+fn main() {
+
+    let s = " Some  string with  too much   whitespaces & around 1% characters that might cause trouble to HTML or LaTeX.";
+    let new_s = remove_whitespaces(s);
+    println!("for HTML: {}", escape_html(new_s.clone()));
+    println!("for LaTeX: {}", escape_tex(new_s));
+    
+    let s = " Une chaîne en français ! On voudrait un résultat « typographiquement correct ».";
+    let new_s = FrenchFormatter::new().format(s);
+    println!("for HTML: {}", escape_nb_spaces(escape_html(new_s.clone())));
+    println!("for LaTeX: {}", escape_nb_spaces_tex(escape_tex(new_s)));
+}
