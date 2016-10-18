21d02a285521f2dcf3ce2983898fb663c62366c5
Elisabeth Henry <liz.henry@ouvaton.org>
2016-10-18 23:19:30 +0200

    Remove is_whitespace function as no longer used

--- src/common.rs
+++ src/common.rs
@@ -5,11 +5,3 @@
 pub const NB_CHAR: char = ' '; // non breaking space
 pub const NB_CHAR_NARROW: char = '\u{202F}'; // narrow non breaking space
 pub const NB_CHAR_EM: char = '\u{2002}'; // demi em space
-
-
-/// Custom function because we don't really want to touch \t or \n
-///
-/// This function detects spaces and non breking spaces
-pub fn is_whitespace(c: char) -> bool {
-    c == ' ' || c == ' ' || c == ' '
-}
