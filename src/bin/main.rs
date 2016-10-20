f407a9f66713cfc439a7b5435881d7056b4ef3bb
Elisabeth Henry <liz.henry@ouvaton.org>
2016-10-20 04:19:23 +0200

    Add binary so it can be used interactively

---
+++ src/bin/main.rs
@@ -0,0 +1,62 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with
+// this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+extern crate crowbook_text_processing;
+
+use crowbook_text_processing::{FrenchFormatter, clean, escape};
+
+use std::env;
+use std::io;
+use std::io::BufRead;
+
+const TOOLS: &'static[(&'static str, &'static str)] = &[
+    ("escape_html", "escape text for HTML display"),
+    ("escape_tex", "escape text for LaTeX display"),
+    ("escape_nb_spaces", "escape non-breaking spaces using HTML entities"),
+    ("escape_nb_spaces_tex", "escape non-breaking spaces using `~` symbol"),
+    ("clean_ellipsis", "use unicode character … for ellipsis"),
+    ("clean_quotes", "try to replace straight quotes with curly ones"),
+    ("format_french", "try to apply french typographic rules"),
+];
+
+fn main() {
+    let args:Vec<_> = env::args()
+        .collect();
+    if args.len() == 1 {
+        println!("\
+{bin} <TRANSFORMATIONS>
+
+Read standard input, sequentially apply each TRANSFORMATION on the text, and print the
+result on standard output.
+
+Valid TRANSFORMATIONS are the following:",
+                 bin = args[0]);
+        for &(name, desc) in TOOLS {
+            println!("    {name}: {desc}",
+                     name = name,
+                     desc = desc);
+        }
+    } else {
+        let french = FrenchFormatter::new();
+
+        let stdin = io::stdin();
+        for line in stdin.lock().lines() {
+            let line = line.expect("Error reading from standard input");
+            let mut output = clean::whitespaces(line);
+            for argument in &args[1..] {
+                output = match argument.as_ref() {
+                    "escape_html" => escape::html(output),
+                    "escape_tex" => escape::tex(output),
+                    "escape_nb_spaces" => escape::nb_spaces(output),
+                    "escape_nb_spaces_tex" => escape::nb_spaces_tex(output),
+                    "clean_quotes" => clean::quotes(output),
+                    "clean_ellipsis" => clean::ellipsis(output),
+                    "format_french" => french.format(output),
+                    t => panic!("transformation {} not recognized", t),
+                }
+            }
+            println!("{}", output);
+        }
+    }
+}
