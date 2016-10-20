d1c08ccce351e50f2d387c56da6228827e9d53fe
Elisabeth Henry <liz.henry@ouvaton.org>
2016-10-20 04:32:01 +0200

    Improve help message

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,34 +9,45 @@ use crowbook_text_processing::{FrenchFormatter, clean, escape};
 use std::env;
 use std::io;
 use std::io::BufRead;
+use std::process::exit;
 
 const TOOLS: &'static[(&'static str, &'static str)] = &[
     ("escape_html", "escape text for HTML display"),
     ("escape_tex", "escape text for LaTeX display"),
     ("escape_nb_spaces", "escape non-breaking spaces using HTML entities"),
-    ("escape_nb_spaces_tex", "escape non-breaking spaces using `~` symbol"),
-    ("clean_ellipsis", "use unicode character … for ellipsis"),
+    ("escape_nb_spaces_tex", "escape non-breaking spaces using ‘~’ symbol"),
+    ("clean_ellipsis", "use unicode character ‘…’ for ellipsis"),
     ("clean_quotes", "try to replace straight quotes with curly ones"),
     ("format_french", "try to apply french typographic rules"),
 ];
+
+fn print_transformations() {
+    for &(name, desc) in TOOLS {
+        println!("    {name}: {desc}",
+                 name = name,
+                 desc = desc);
+    }
+}
 
 fn main() {
     let args:Vec<_> = env::args()
         .collect();
     if args.len() == 1 {
         println!("\
-{bin} <TRANSFORMATIONS>
+{bin} {version}
+
+USAGE: {bin} <TRANSFORMATIONS>
 
 Read standard input, sequentially apply each TRANSFORMATION on the text, and print the
 result on standard output.
 
-Valid TRANSFORMATIONS are the following:",
-                 bin = args[0]);
-        for &(name, desc) in TOOLS {
-            println!("    {name}: {desc}",
-                     name = name,
-                     desc = desc);
-        }
+Valid transformations are the following:",
+                 bin = env!("CARGO_PKG_NAME"),
+                 version = env!("CARGO_PKG_VERSION"));
+        print_transformations();
+        println!();
+        println!("EXAMPLE: {bin} clean_quotes clean_ellipsis escape_html",
+                 bin = env!("CARGO_PKG_NAME"));
     } else {
         let french = FrenchFormatter::new();
 
@@ -53,7 +64,12 @@ Valid TRANSFORMATIONS are the following:",
                     "clean_quotes" => clean::quotes(output),
                     "clean_ellipsis" => clean::ellipsis(output),
                     "format_french" => french.format(output),
-                    t => panic!("transformation {} not recognized", t),
+                    t => {
+                        println!("Error: transformation “{}” not recognized.", t);
+                        println!("Valid transformations are:");
+                        print_transformations();
+                        exit(0);
+                    },
                 }
             }
             println!("{}", output);
